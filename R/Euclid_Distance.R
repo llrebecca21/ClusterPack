bede13e71ab4321565b63f6e082bc784e2d52242
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-17 14:30:56 -0600

    Change the roxygen skeleton to represent the changes in the input variable; change dataf to mat_d to highlight the expectation a matrix will be inputted

--- R/Euclid_Distance.R
+++ R/Euclid_Distance.R
@@ -1,26 +1,26 @@
 #' Euclid_Distance function
 #'
-#' @param dataf A numeric dataframe or a matrix of dimension n x p. Assuming last column of dataframe is classifying vector
+#' @param mat_d
 #'
 #' @return
 #' @export
 #'
 #' @examples
-Euclid_Distance <- function(dataf){
+Euclid_Distance <- function(mat_d){
   #Compute the Euclidean Distance of a Matrix
   #Calculates by calculating the distance between the first row and each row in the matrix
 
   #Initialize dimensions of the dataframe
   #dataframe is an n x p matrix after turning into matrix form
-  n <- nrow(dataf)
-  p <- ncol(dataf)
+  n <- nrow(mat_d)
+  p <- ncol(mat_d)
 
   #Initialize vector that will store the distances
   dist_Euc <- rep(0,n)
 
   #Run for loop over the rows and calculate the distance
   for(i in 1:n){
-    dist_Euc[i] <- sqrt(sum((dataf[1,] - dataf[i,])^2))
+    dist_Euc[i] <- sqrt(sum((mat_d[1,] - mat_d[i,])^2))
   }
   #Return Euclidean Distance vector
   return(dist_Euc)
