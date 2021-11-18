e748213d08e24d25788461ba12f06378e86fff33
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-18 12:00:07 -0600

    Change the name of the function to Euclid_Distance_c to distinguish from the R version

--- src/Euclid_Distance.cpp
+++ src/Euclid_Distance.cpp
@@ -2,7 +2,7 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-arma::colvec Euclid_Distance(const arma::mat& mat_d){
+arma::colvec Euclid_Distance_c(const arma::mat& mat_d){
 //Compute the Euclidean Distance of a Matrix
 //Calculates by calculating the distance between the first row and each row in the matrix
 
