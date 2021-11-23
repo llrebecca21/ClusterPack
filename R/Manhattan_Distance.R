1170ea0fc4bdd034423b11c3b891933a74efca08
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-23 12:14:57 -0600

    Write pseudocode for Manhattan distance function

--- R/Manhattan_Distance.R
+++ R/Manhattan_Distance.R
@@ -1,5 +1,13 @@
 
 
+Man_distance <- function(v1,v2){
+  # Check that the length of the vectors v1 and v2 are the same length
+
+  # Calculate Manhattan Distance between two vectors:
+  # \sum(|a-b|)
+
+}
+
 
 
 
