9551ff6f89e14504f5b5b31f5190c1a64b0477dc
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-23 12:18:42 -0600

    Write basic code for simple version of Manhattan distance for just between two vectors

--- R/Manhattan_Distance.R
+++ R/Manhattan_Distance.R
@@ -2,10 +2,13 @@
 
 Man_distance <- function(v1,v2){
   # Check that the length of the vectors v1 and v2 are the same length
-
-  # Calculate Manhattan Distance between two vectors:
-  # \sum(|a-b|)
-
+  if (length(v1) != length(v2)){
+    stop('Length of the vectors needs to be equal')
+  }else{
+    # Calculate Manhattan Distance between two vectors:
+    # \sum(|a-b|)
+    man_dist_vec <- sum(abs(a-b))
+  }return(man_dist_vec)
 }
 
 
