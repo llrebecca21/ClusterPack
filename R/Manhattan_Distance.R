7b0c226cc2bdd5f2f6700c3a904e5d21604bdf81
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-23 12:22:21 -0600

    Create roxygen skeleton for Manhattan_distance function; rename the function as Manhattan_distance

--- R/Manhattan_Distance.R
+++ R/Manhattan_Distance.R
@@ -1,6 +1,13 @@
-
-
-Man_distance <- function(v1,v2){
+#' Manhattan_distance function
+#'
+#' @param v1 vector of length 1 x p
+#' @param v2 vector of length 1 x p
+#'
+#' @return vector of length 1 x 1 that computes the distance between v1 and v2
+#' @export
+#'
+#' @examples
+Manhattan_distance <- function(v1,v2){
   # Check that the length of the vectors v1 and v2 are the same length
   if (length(v1) != length(v2)){
     stop('Length of the vectors needs to be equal')
@@ -8,7 +15,8 @@ Man_distance <- function(v1,v2){
     # Calculate Manhattan Distance between two vectors:
     # \sum(|a-b|)
     man_dist_vec <- sum(abs(a-b))
-  }return(man_dist_vec)
+  }
+  return(man_dist_vec)
 }
 
 
