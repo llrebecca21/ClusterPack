4ffce21c4c51bd32a6c0a7ebf17f0b8cbd9521a5
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-23 12:27:47 -0600

    Create Minkowski Distance file that will store Minkowski Distance function. Create pseudocode for the simple Minkowski_distance function that is used between two vectors

---
+++ R/Minkowski_Distance.R
@@ -0,0 +1,24 @@
+Minkowski_distance <- function(v1,v2,p){
+  #Check that the p value is a viable input (greater than zero)
+
+  #Check that the lengths of v1 and v2 are compatible
+
+  #Calculate Minkowski distance between the two vectors
+  # \sum(|a-b|^p)^(1/p)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
