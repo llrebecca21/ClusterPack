d536c5580c8bd904efe77fb2a8c1f02d1939e8fc
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-23 18:44:31 -0600

    Fill out Minkowski distance function with checks for compatibility of the inputs of the vectors; return the vector of the distances calculated

--- R/Minkowski_Distance.R
+++ R/Minkowski_Distance.R
@@ -1,13 +1,20 @@
 Minkowski_distance <- function(v1,v2,p){
   #Check that the p value is a viable input (greater than zero)
+  if(p <= 0){
+    stop('p must be greater than zero')
 
-  #Check that the lengths of v1 and v2 are compatible
-
-  #Calculate Minkowski distance between the two vectors
-  # \sum(|a-b|^p)^(1/p)
-
+  }if(length(v1) != length(v2)){
+    stop('The length of the vectors need to be equal')
+  }if(p == Inf){
+    #Calculate Sup-norm
+    return(max(abs(a-b)))
+  }else{
+    #Calculate Minkowski distance between the two vectors
+    # \sum(|a-b|^p)^(1/p)
+    Mink_dist_vec <- sum(abs(a-b)^p)^(1/p)
+  }
+  return(Mink_dist_vec)
 }
-
 
 
 
