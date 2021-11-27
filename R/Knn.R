c5eb624c6327f62608a7959ee48a1e9901fc362c
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-27 17:13:32 -0600

    create first draft of the KNN algorithm with Euclidean Distance

--- R/Knn.R
+++ R/Knn.R
@@ -15,14 +15,17 @@ Knn <- function(X,observation,K){
   if(ncol(X) != ncol(observation)){
     stop("The data does not have the same number of variables!")
   }
-  #Calculate the distance by calling whichever distance function is called
-  if(dist = Euclid){
-    dist = apply(X, 1, )
-  }
+  #Calculate the distance by calling the Euclidean distance function
+  dist <- sapply(X,Euclidean_Distance_Gen(v1,v2))
+
   #Calculate the closest neighbors
-    #sort the distances calculated
+  #sort the distances calculated
+  dist_ord <- sort(dist)[1:K]
+
+  index_neighb <- which(dist %in% dist_ord)
 
   #Return the K data points that have the smallest distance
+  return(list(index_neighb, dist_ord))
 }
 
 
