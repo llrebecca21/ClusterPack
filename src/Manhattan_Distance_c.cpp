96080a248ab25c22fb8aeb3391f8a1530a48edd7
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-26 19:53:46 -0600

    Add // [[Rcpp::export]] to the top of the function

--- src/Manhattan_Distance_c.cpp
+++ src/Manhattan_Distance_c.cpp
@@ -1,6 +1,7 @@
 #include "RcppArmadillo.h"
 using namespace Rcpp;
 
+// [[Rcpp::export]]
 arma::colvec Manhattan_Distance_c(const arma::uvec& v1, const arma::uvec& v2 ){
   //Compute Manhattan Distance between vectors v1 and v2
   //initialize the vector that will store the distances
