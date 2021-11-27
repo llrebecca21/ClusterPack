9976cfa5e28d2f3add4e6061291aee6f71cc50f4
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-26 19:41:12 -0600

    Fill in first draft of c++ version of Manhattan_Distance_c function that calculates the manhattan distance between two vectors

--- src/Manhattan_Distance_c.cpp
+++ src/Manhattan_Distance_c.cpp
@@ -3,6 +3,13 @@ using namespace Rcpp;
 
 arma::colvec Manhattan_Distance_c(const arma::uvec& v1, const arma::uvec& v2 ){
   //Compute Manhattan Distance between vectors v1 and v2
+  //initialize the vector that will store the distances
+  arma::colvec man_dist_vec;
+
+  //Compute the distances
+  man_dist_vec = arma::sum(abs(v1-v2));
+
+  return(man_dist_vec);
 }
 
 
