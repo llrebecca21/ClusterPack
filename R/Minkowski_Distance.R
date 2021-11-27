969945acb93896c938d03c9cc7719c983fb1e152
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-26 19:55:58 -0600

    Fix error with naming of vectors in the calculation of Minkowski distance

--- R/Minkowski_Distance.R
+++ R/Minkowski_Distance.R
@@ -17,11 +17,11 @@ Minkowski_distance <- function(v1,v2,p){
     stop('The length of the vectors need to be equal')
   }if(p == Inf){
     #Calculate Sup-norm
-    return(max(abs(a-b)))
+    return(max(abs(v1-v2)))
   }else{
     #Calculate Minkowski distance between the two vectors
     # \sum(|a-b|^p)^(1/p)
-    Mink_dist_vec <- sum(abs(a-b)^p)^(1/p)
+    Mink_dist_vec <- sum(abs(v1-v2)^p)^(1/p)
   }
   return(Mink_dist_vec)
 }
