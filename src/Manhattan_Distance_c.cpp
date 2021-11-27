3fd6c2252b3acfc04dc32e1e8d2e5ceb29a7e568
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-26 19:34:06 -0600

    Create cpp file to contain manhattan distance computation between two vectors

---
+++ src/Manhattan_Distance_c.cpp
@@ -0,0 +1,14 @@
+#include "RcppArmadillo.h"
+using namespace Rcpp;
+
+arma::colvec Manhattan_Distance_c(const arma::uvec& v1, const arma::uvec& v2 ){
+  //Compute Manhattan Distance between vectors v1 and v2
+}
+
+
+
+
+
+
+
+
