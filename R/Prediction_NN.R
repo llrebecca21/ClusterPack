c853e06fc9bd309007e06fb1dfbe9d398aaf667c
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-28 15:51:51 -0600

    Repeat the same calculation for prediction if the X[,Y] is of character type

--- R/Prediction_NN.R
+++ R/Prediction_NN.R
@@ -7,7 +7,10 @@ Prediction_NN <- function(X,Y, weights = NULL){
     pred <- names(group[group == max(group)])
   }
   #If X[, Y] is a character type:
-
+  if(is.character(X[ ,Y])){
+    group <- table(X[ ,Y])
+    pred <- names(group[group == max(group)])
+  }
   #If X[, Y] is a numeric type:
       #Calculate using weights if provided
       #If not weights are provided calculate the standard way
