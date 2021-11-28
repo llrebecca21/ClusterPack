5f1af3a76ec814c2eaa69212fc8a68f1c3057a08
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-28 15:38:51 -0600

    Create Psuedocode for the prediction calculations

--- R/KNN_Euc.R
+++ R/KNN_Euc.R
@@ -28,6 +28,10 @@ KNN_Euc <- function(X_test ,X_pred, Y, K){
 
     nearest <- Nearest_Neighbors(X, X_pred[i, -y_index], K)
 
+    # Calculate predictions
+    # Create a function for the prediction calculations
+    predictions <-
+
 
   }
 
