35b592269f8c0a20447e4823f08b3d42f5397086
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-28 13:17:42 -0600

    Delete previous code for Euclidean Distance function optimized in C++

--- src/Euclid_Distance_c.cpp
+++ src/Euclid_Distance_c.cpp
@@ -6,18 +6,7 @@ arma::colvec Euclid_Distance_c(const arma::mat& mat_d){
 //Compute the Euclidean Distance of a Matrix
 //Calculates by calculating the distance between the first row and each row in the matrix
 
-//Initialize dimensions of the dataframe
-//dataframe is an n x p matrix after turning into matrix form
-  int n = mat_d.n_rows;
-  int p = mat_d.n_cols;
 
-//Initialize vector that will store the distances
-    arma::colvec dist_Euc(n, arma::fill::zeros);
-
-//Run for loop over the rows and calculate the distance
-      for(int i = 0; i < n; i++){
-        dist_Euc(i) = sqrt(arma::sum(square(mat_d.row(0) - mat_d.row(i))));
-      }
 //Return Euclidean Distance vector
       return(dist_Euc);
 }
