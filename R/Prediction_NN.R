9ec3c772510157379b56c54f6e13aa7fd21d83c8
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-28 15:45:47 -0600

    Create Pseudocode for Prediction_NN function

--- R/Prediction_NN.R
+++ R/Prediction_NN.R
@@ -1,5 +1,15 @@
-Prediction_NN <- function(){
+Prediction_NN <- function(X,Y, weights = NULL){
+  #Make sure X is a matrix
 
+  #If X[, Y] is a factor type:
+
+  #If X[, Y] is a character type:
+
+  #If X[, Y] is a numeric type:
+      #Calculate using weights if provided
+      #If not weights are provided calculate the standard way
+
+  #If no prediction is given, give a warning
 }
 
 
