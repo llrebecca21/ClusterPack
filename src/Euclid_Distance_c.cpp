401618e21b5b0700272d9132928d6e5ead06478d
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-28 18:42:03 -0600

    Fix bug in Euclidean distance function optimized in c++. Fix the output for the function

--- src/Euclid_Distance_c.cpp
+++ src/Euclid_Distance_c.cpp
@@ -2,11 +2,10 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-arma::colvec Euclid_Distance_c(const arma::mat& mat_d){
+double Euclid_Distance_c(const arma::uvec& v1, const arma::uvec& v2){
 //Compute the Euclidean Distance of a Matrix
 //Calculates by calculating the distance between the first row and each row in the matrix
-
-
+      double dist_Euc = sqrt(pow(arma::accu(v1-v2),2));
 //Return Euclidean Distance vector
       return(dist_Euc);
 }
