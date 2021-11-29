561fc0957c3eb90dfb2006a85d6c3c7eeb7a8c84
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-28 19:18:03 -0600

    Fix the error in the Euclidean function optimized in C++ where it only returned integer valued distances

--- src/Euclid_Distance_c.cpp
+++ src/Euclid_Distance_c.cpp
@@ -2,10 +2,21 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-double Euclid_Distance_c(const arma::uvec& v1, const arma::uvec& v2){
+double Euclid_Distance_c(const arma::colvec& v1, const arma::colvec& v2){
 //Compute the Euclidean Distance of a Matrix
 //Calculates by calculating the distance between the first row and each row in the matrix
-      double dist_Euc = sqrt(pow(arma::accu(v1-v2),2));
+      double vec_sum = accu(v1 - v2);
+      double vec_sq  = pow(vec_sum, 2);
+      double dist_Euc = sqrt(vec_sq);
 //Return Euclidean Distance vector
       return(dist_Euc);
 }
+
+/***R
+set.seed(21)
+v1 <- c(rnorm(1:10))
+v2 <- c(rnorm(1:10))
+
+print(Euclid_Distance_c(v1,v2))
+
+*/
