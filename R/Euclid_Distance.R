c73726566b9fc1fbf6e76f85df74d93d13b57958
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-28 19:29:11 -0600

    delete the version of the Euclidean_distance function that took in matrices or dataframes. Change to take in two vectors as arguments. optimize the function so that it calculates the distance in a cpp file

--- R/Euclid_Distance.R
+++ R/Euclid_Distance.R
@@ -1,26 +1,19 @@
-#' Euclid_Distance function
+#' Euclidean Distance
 #'
-#' @param mat_d n x p matrix that the Euclidean Distance will be run on
+#' @param v1
+#' @param v2
 #'
-#' @return a vector of length n containing Euclidean Distances between first row and subsequent rows
+#' @return
 #' @export
 #'
 #' @examples
-Euclid_Distance <- function(mat_d){
-  #Compute the Euclidean Distance of a Matrix
-  #Calculates by calculating the distance between the first row and each row in the matrix
-
-  #Initialize dimensions of the dataframe
-  #dataframe is an n x p matrix after turning into matrix form
-  n <- nrow(mat_d)
-
-  #Initialize vector that will store the distances
-  dist_Euc <- rep(0,n)
-
-  #Run for loop over the rows and calculate the distance
-  for(i in 1:n){
-    dist_Euc[i] <- sqrt(sum((mat_d[1,] - mat_d[i,])^2))
+Euclid_Distance <- function(v1, v2){
+  #Check for same length of the vectors
+  if(length(v1)!= length(v2)){
+    stop("The vectors are not of the same length")
+  }else{
+    dist_Euc <- Euclid_Distance_c(v1,v2)
+    #dist_Euc <- sqrt(sum(v1-v2)^2)
   }
-  #Return Euclidean Distance vector
   return(dist_Euc)
 }
