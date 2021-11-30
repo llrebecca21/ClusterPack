68f2655d44f4ed3a5b9339001e43a15df7a9b903
Rebecca Lee <llrebecca21@tamu.edu>
2021-11-30 10:32:00 -0600

    Change the Euclidean distance function to just run the R version to make sure the algorithm does work. Will change back to C++ version once bugs have been fixed in algorithm

--- R/Euclid_Distance.R
+++ R/Euclid_Distance.R
@@ -12,8 +12,8 @@ Euclid_Distance <- function(v1, v2){
   if(length(v1)!= length(v2)){
     stop("The vectors are not of the same length")
   }else{
-    dist_Euc <- Euclid_Distance_c(v1,v2)
-    #dist_Euc <- sqrt(sum(v1-v2)^2)
+    #dist_Euc <- Euclid_Distance_c(v1,v2)
+    dist_Euc <- sqrt(sum(v1-v2)^2)
   }
   return(dist_Euc)
 }
