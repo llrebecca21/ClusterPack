f694037a00339ef401ca3d7fa0e57bf5969025bd
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-01 11:55:50 -0600

    Comment out unneeded print statements for debugging; Finished debugging C++ version of Euc_distance function so can call this function in the wrapper;

--- R/Euclid_Distance.R
+++ R/Euclid_Distance.R
@@ -9,11 +9,13 @@
 #' @examples
 Euclid_Distance <- function(v1, v2){
   #Check for same length of the vectors
+  #print(str(v1))
+  #print((v2))
   if(length(v1)!= length(v2)){
     stop("The vectors are not of the same length")
   }else{
-    #dist_Euc <- Euclid_Distance_c(v1,v2)
-    dist_Euc <- sqrt(sum(v1-v2)^2)
+    dist_Euc <- Euclid_Distance_c(v1,v2)
+    #dist_Euc <- sqrt(sum(v1-v2)^2)
   }
   return(dist_Euc)
 }
