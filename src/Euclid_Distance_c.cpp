a59f49e7222240457e13c6fbe7cb2514b324861a
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-01 12:36:12 -0600

    Delete old version of Euclid_Distance_c and replace with vectorized version;

--- src/Euclid_Distance_c.cpp
+++ src/Euclid_Distance_c.cpp
@@ -2,12 +2,14 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-double Euclid_Distance_c(const arma::colvec& v1, const arma::colvec& v2){
+arma::colvec Euclid_Distance_c(const arma::mat& X, const arma::rowvec& v){
 //Compute the Euclidean Distance of a Matrix
 //Calculates by calculating the distance between the first row and each row in the matrix
-      double vec_sum = accu(v1 - v2);
-      double vec_sq  = pow(vec_sum, 2);
-      double dist_Euc = sqrt(vec_sq);
+      arma::mat m1 = X.each_row() - v;
+                m1 = square(m1);
+      arma::colvec d1 = sum(m1, 1);
+                   d1 = sqrt(d1);
+
 //Return Euclidean Distance vector
-      return(dist_Euc);
+      return(d1);
 }
