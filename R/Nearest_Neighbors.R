60f9305e8d348e57fa30325258c05476335bf3f8
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-02 10:55:45 -0600

    Add new argument for method chosen in original KNN function to determine how to calculate the distance

--- R/Nearest_Neighbors.R
+++ R/Nearest_Neighbors.R
@@ -1,12 +1,20 @@
-Nearest_Neighbors <- function(X, observation, K){
+Nearest_Neighbors <- function(X, observation, K, method){
   #Calculate the nearest neighbors
 
   #X is the X_test matrix
   #observation is a single row of X_pred
 
-  # Calculate the distance by calling the Euclidean distance function
-  # Apply Euclid_Distance_c over the rows of X
-  dist <- Euclid_Distance_c(X, observation)
+  # Calculate the distance by calling the distance function requested in method argument
+  if(method == "Euc"){
+    # Apply Euclid_Distance_c over the rows of X
+    dist <- Euclid_Distance_c(X, observation)
+  }
+
+  if(method == "Man"){
+    #Apply Manhattan_Distance_c over the rows of X
+    dist <- Manhattan_Distance_c(X, observation)
+  }
+
 
   #Calculate the closest neighbors
   #sort the distances calculated
