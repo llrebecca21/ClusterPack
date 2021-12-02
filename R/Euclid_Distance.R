7a0b77250e17cbd5e1b6a45fa55243a97c0423da
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-01 20:04:30 -0600

    Change input variables for function to X and v

--- R/Euclid_Distance.R
+++ R/Euclid_Distance.R
@@ -7,14 +7,14 @@
 #' @export
 #'
 #' @examples
-Euclid_Distance <- function(v1, v2){
+Euclid_Distance <- function(X, v){
   #Check for same length of the vectors
   #print(str(v1))
   #print((v2))
-  if(length(v1)!= length(v2)){
+  if(ncol(X)!= length(v)){
     stop("The vectors are not of the same length")
   }else{
-    dist_Euc <- Euclid_Distance_c(v1,v2)
+    dist_Euc <- Euclid_Distance_c(X,v)
     #dist_Euc <- sqrt(sum((v1-v2)^2))
   }
   return(dist_Euc)
