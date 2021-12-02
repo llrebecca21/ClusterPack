7d14d71533cba210b17764d152b734c0c10b4c06
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-01 19:10:37 -0600

    Delete unneeded code; change the inputs for calling Nearest_Neighbors; change the inputs for Prediction_NN

--- R/KNN_Euc.R
+++ R/KNN_Euc.R
@@ -22,22 +22,19 @@ KNN_Euc <- function(X_test ,X_pred, Y_test, Y_pred, K, pred_weights = FALSE){
   #Initialize a prediction vector and any other variables before for loop
   m = nrow(X_pred)
   predict_vec <- c()
-  X_test <- X_test[ , -ncol(X_test)]
-
-  X_pred <- as.matrix(X_pred)
 
   #Create for loop to calculate the predictions by calculating nearest neighbors
   for(i in 1:m){
 
     #Calculate distances and nearest neighbors by calling Nearest_Neighbors function
     #Nearest_Neighbors returns a list with distances and index_of_neighbors
-    nearest <- Nearest_Neighbors(X = X, observation = X_pred[i, -], K = K)
+    nearest <- Nearest_Neighbors(X = X_test, observation = X_pred[i, ], K = K)
 
     # Calculate predictions by calling Prediction_NN function
     if(pred_weights == TRUE){
-      predict_vec[i] <- Prediction_NN(X = X_test[nearest[[1]], ], Y = Y, weights = nearest[[2]])
+      predict_vec[i] <- Prediction_NN(X = X_test[nearest[[1]], ], Y = Y_test, weights = nearest[[2]])
     }else{
-      predict_vec[i] <- Prediction_NN(X = X_test[nearest[[1]], ], Y = Y)
+      predict_vec[i] <- Prediction_NN(X = X_test[nearest[[1]], ], Y = Y_test)
     }
   }
   return(predict_vec)
