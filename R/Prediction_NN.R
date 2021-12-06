53594d1cbd9c204a9ce7cadac37f9af91f8331c9
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-06 17:36:30 -0600

    Style Prediciton_NN function using add-in

--- R/Prediction_NN.R
+++ R/Prediction_NN.R
@@ -1,24 +1,24 @@
-Prediction_NN <- function(X,Y, weights = NULL){
+Prediction_NN <- function(X, Y, weights = NULL) {
 
   # Description of inputs:
   # X = X_test[nearest[[1]]
   # Y = Y_test
 
-  #If Y is a factor type:
-  if(is.factor(Y) | is.character(Y)){
+  # If Y is a factor type:
+  if (is.factor(Y) | is.character(Y)) {
     group <- table(Y)
     pred <- names(group[group == max(group)])[1]
   }
 
-  #If Y is a numeric type:
-  if(is.numeric(Y)){
-    #Calculate using weights if provided
-    #determine easier way to state this
-    if(is.null(weights) == FALSE){
+  # If Y is a numeric type:
+  if (is.numeric(Y)) {
+    # Calculate using weights if provided
+    # determine easier way to state this
+    if (is.null(weights) == FALSE) {
       Weight_give <- 1 / (weights)
       pred <- weighted.mean(Y, Weight_give)
-      #If not, weights are provided calculate the standard way
-    } else{
+      # If not, weights are provided calculate the standard way
+    } else {
       pred <- mean(Y)
     }
   }
