009abb40b46e7c7f64c5140f5f01f05732ebd28c
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-06 17:34:13 -0600

    Style Nearest_Neighbors function using add-in

--- R/Nearest_Neighbors.R
+++ R/Nearest_Neighbors.R
@@ -1,32 +1,32 @@
-Nearest_Neighbors <- function(X, observation, K, method, p = NULL){
-  #Calculate the nearest neighbors
+Nearest_Neighbors <- function(X, observation, K, method, p = NULL) {
+  # Calculate the nearest neighbors
 
-  #X is the X_test matrix
-  #observation is a single row of X_pred
+  # X is the X_test matrix
+  # observation is a single row of X_pred
 
   # Calculate the distance by calling the distance function requested in method argument
-  if(method == "Euc"){
+  if (method == "Euc") {
     # Apply Euclid_Distance_c over the rows of X
     dist <- Euclid_Distance_c(X, observation)
   }
 
-  if(method == "Man"){
-    #Apply Manhattan_Distance_c over the rows of X
+  if (method == "Man") {
+    # Apply Manhattan_Distance_c over the rows of X
     dist <- Manhattan_Distance_c(X, observation)
   }
 
-  if(method == "Min"){
-    #Apply Minkowski_Distance_c over the rows of X
+  if (method == "Min") {
+    # Apply Minkowski_Distance_c over the rows of X
     dist <- Minkowski_Distance_c(X, observation, p)
   }
 
-  if(method == "Cos"){
-    #Apply Cos_Distance_c over the rows of X
+  if (method == "Cos") {
+    # Apply Cos_Distance_c over the rows of X
     dist <- Cos_Distance_c(X, observation)
   }
 
-  #Calculate the closest neighbors
-  #sort the distances calculated
+  # Calculate the closest neighbors
+  # sort the distances calculated
   distances <- sort(dist)[1:K]
   index_neighb <- which(dist %in% distances)
 
