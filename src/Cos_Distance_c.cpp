a740f11d563196b47818d88e7741b3089f58314a
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-06 17:29:56 -0600

    Delete unneeded comments; Delete the example to check for functionality

--- src/Cos_Distance_c.cpp
+++ src/Cos_Distance_c.cpp
@@ -3,10 +3,6 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 arma::colvec Cos_Distance_c(arma::mat& X, arma::rowvec& v){
-  //numerator <- colSums(t(X) * v)
-  //denominator <- sqrt(rowSums(X * X) * sum(b*b))
-  //return(1 - (numerator/denominator))
-
 
   // Create C++ version of this distance equation
   arma::mat mult = X.each_row() % v;
@@ -16,14 +12,3 @@ arma::colvec Cos_Distance_c(arma::mat& X, arma::rowvec& v){
 
   return(cos_vec);
 }
-
-/***R
-X = matrix(rnorm(1:10), 5)
-print(X)
-v = rnorm(1:2)
-print(v)
-
-print(Cos_Distance_c(X, v))
-*/
-
-
