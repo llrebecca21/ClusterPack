03e7956060a39217f4be369cd3eaaf0f98462ded
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-07 11:41:18 -0600

    Create first draft of Jaccard Distance function where the Jaccard coefficient is calculated between the rows of X and the vector y; Create Roxygen Skeleton for documentation of the function.

---
+++ R/Jaccard_Distance.R
@@ -0,0 +1,21 @@
+#' Jaccard Distance function
+#'
+#' @param X n x m matrix
+#' @param y 1 x m vector
+#'
+#' @return 1 x m vector of (Jaccard coefficient) distances between rows of X and vector y
+#' @export
+#'
+#' @examples
+Jaccard_Distance <- function(X, y){
+  A <- length(X)
+  B <- length(y)
+  inter_Xy <- length(intersection(X,y))
+  un_Xy <- A + B - lint_Xy
+  return(inter_Xy/un_Xy)
+}
+
+
+
+
+
