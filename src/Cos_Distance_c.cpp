7fbe8e9e8cb8d4442dc00b52dc9b73047368bdc9
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-06 18:15:02 -0600

    Make comment clearer about what the function is calculating

--- src/Cos_Distance_c.cpp
+++ src/Cos_Distance_c.cpp
@@ -4,7 +4,7 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 arma::colvec Cos_Distance_c(arma::mat& X, arma::rowvec& v){
 
-  // Create C++ version of this distance equation
+  // Create C++ version of Cos-Similarity distance equation
   arma::mat mult = X.each_row() % v;
   arma::colvec dot_prod = sum(mult, 1);
   arma::colvec denom = sqrt(sum(X % X, 1)) * arma::norm(v);
