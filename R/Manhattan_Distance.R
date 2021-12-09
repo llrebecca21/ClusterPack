3040513540a85bd51086d5ac68e64298de0770b4
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-09 17:54:19 -0600

    Create an example for the Manhattan_Distance(X,v) function in the documentation

--- R/Manhattan_Distance.R
+++ R/Manhattan_Distance.R
@@ -7,12 +7,16 @@
 #' @export
 #'
 #' @examples
+#' set.seed(101)
+#' X = matrix(c(1,17,19,21,1,0), nrow = 3)
+#' v = c(10,15,3)
+#' Manhattan_Distance(X,v)
 Manhattan_Distance <- function(X,v){
   # Check that the length of the vectors v1 and v2 are the same length
   if (nrow(X) != length(v)){
     stop('The number of rows of X needs to be equal to the length of v')
   }else{
-    # Calculate Manhattan Distance between two vectors:
+    # Calculate Manhattan Distance between the rows of X and the vector v:
     # \sum(|a-b|)
     man_dist_vec <- Manhattan_Distance_c(X,v)
   }
