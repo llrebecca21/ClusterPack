7819050be7483483debdfa934423c00864933ffa
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-09 22:34:26 -0600

    Delete unneeded parameter Y_pred.

--- R/KNN_Euc.R
+++ R/KNN_Euc.R
@@ -2,8 +2,7 @@
 #'
 #' @param X_test n x m numeric dataframe or matrix
 #' @param X_pred r x m numeric dataframe or matrix
-#' @param Y_test n x 1 dataframe or matrix label column which is the label column for the X_test dataframe
-#' @param Y_pred r x 1 dataframe or matrix label column which is the label column for the X_pred dataframe
+#' @param Y_test n x 1 column vector of test observation labels
 #' @param K      integer value; the number of neighbors
 #' @param method Determines how distance will be calculated for nearest-neighbors. Choose from the following:
 #'               "Euc": will calculate using the Euclidean Distance
@@ -27,15 +26,14 @@
 #' X_pred = iris[train_ind, -ncol(iris)]
 #'
 #' Y_test = iris[-train_ind, ncol(iris)]
-#' Y_pred = iris[train_ind, ncol(iris)]
 #'
-#' KNN_Euc(X_test = X_test, X_pred = X_pred, Y_test = Y_test, Y_pred = Y_pred, K = 5)
+#' KNN_Euc(X_test = X_test, X_pred = X_pred, Y_test = Y_test, K = 5)
 #'
 #' ## returns the following:
 #' ## [1] "setosa"   "versicolor"   "virginica"   "virginica"  "virginica"   "virginica"
 #' ## [7] "virginica"   "virginica"   "versicolor"   "virginica"
 #'
-KNN_Euc <- function(X_test, X_pred, Y_test, Y_pred, K, method = c("Euc", "Man", "Min", "Cos"), pred_weights = FALSE, p = NULL) {
+KNN_Euc <- function(X_test, X_pred, Y_test, K, method = c("Euc", "Man", "Min", "Cos"), pred_weights = FALSE, p = NULL) {
   if ((K %% 1 != 0)) {
     stop("K needs to be an integer")
   }
@@ -58,15 +56,11 @@ KNN_Euc <- function(X_test, X_pred, Y_test, Y_pred, K, method = c("Euc", "Man", 
     X_pred <- as.matrix(X_pred)
   }
 
-  Y_pred <- as.vector(Y_pred)
+
 
   # compatibility checks for dimensionality
   if (nrow(X_test) != length(Y_test)) {
     stop("The number of rows of X_test does not match the length of Y_test")
-  }
-
-  if (nrow(X_pred) != length(Y_pred)) {
-    stop("The number of rows of X_pred does not match the length of Y_pred")
   }
 
   if (ncol(X_test) != ncol(X_pred)) {
