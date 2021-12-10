2cc1e288a38d1e70521ab47a944194c0aca7bf47
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-09 18:07:29 -0600

    Create example for documentation

--- R/Cos_Distance.R
+++ R/Cos_Distance.R
@@ -1,4 +1,4 @@
-#' Cosine_Distance function
+#' Cos_Distance function
 #'
 #' @param X n x m matrix
 #' @param v 1 x m vector
@@ -7,6 +7,9 @@
 #' @export
 #'
 #' @examples
+#' X = matrix(c(1,1,0,1,1,0), nrow = 2, byrow = FALSE)
+#' v = c(0,0,1)
+#' Cos_Distance(X,v)
 Cos_Distance = function(X,v){
   #Calculate the distance between the rows of X and the vector v
   #Call the Cos_Distance_c function to calculate the distance
