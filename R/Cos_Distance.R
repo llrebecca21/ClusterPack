cb7a363c4e40aee115ba1e0615940b4e80445e4a
Rebecca Lee <llrebecca21@tamu.edu>
2021-12-09 20:04:49 -0600

    Update the roxygen documentation of the function and state the output of the example

--- R/Cos_Distance.R
+++ R/Cos_Distance.R
@@ -3,13 +3,16 @@
 #' @param X n x m matrix
 #' @param v 1 x m vector
 #'
-#' @return vector of (Cosine Similarity) Distances calculated between the rows of X and the vector v
+#' @return column matrix of the (Cosine Similarity) Distances calculated between the rows of X and the vector v
 #' @export
 #'
 #' @examples
 #' X = matrix(c(1,1,0,1,1,0), nrow = 2, byrow = FALSE)
 #' v = c(0,0,1)
 #' Cos_Distance(X,v)
+#' # Returns the following:
+#' # [0.2928932, 1.0000000]'
+#'
 Cos_Distance = function(X,v){
   #Calculate the distance between the rows of X and the vector v
   #Call the Cos_Distance_c function to calculate the distance
